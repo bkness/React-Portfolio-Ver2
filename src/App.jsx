48f5e4ab74e59578a42bb774273ae38fb141538b
bkness <kbrandon863@gmail.com>
2024-06-08 00:53:28 -0700

    netlify form test

--- src/App.jsx
+++ src/App.jsx
@@ -12,34 +12,7 @@ function App() {
     <>
       <NavTabs />
       <main className="main">
-      <div>
-      <form name='contact v1' method='POST' data-netlify='true' onSubmit='submit'> 
-   
-    <input type="hidden" name="form-name" value='contact v1' />
-   
-    <div>
-      <label>First Name <br />
-        <input type="text" name='first-name' />
-      </label>
-    </div>
-
-    <div>
-      <label htmlFor='email'>Email</label> <br /> 
-      <input id='email' type="email" name="email" />
-    </div>
-
-    <div>
-      <label>Messages <br />
-      <textarea name="messages" rows='7'></textarea>
-    
-      </label>
-    </div>
-
-    <button type='submit'>Submit The Results</button>
-    
-
-      </form>
-    </div>
+        
         <Outlet />
       </main>
       <Footer />
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,32 +4,28 @@ import React from 'react';
 export default function Contact() {
   return (
     <div>
-      <form name='contact v1' method='POST' data-netlify='true' onSubmit='submit'> 
-   
-    <input type="hidden" name="form-name" value='contact v1' />
-   
-    <div>
-      <label>First Name <br />
-        <input type="text" name='first-name' />
-      </label>
-    </div>
-
-    <div>
-      <label htmlFor='email'>Email</label> <br /> 
-      <input id='email' type="email" name="email" />
-    </div>
-
-    <div>
-      <label>Messages <br />
-      <textarea name="messages" rows='7'></textarea>
-    
-      </label>
-    </div>
-
-    <button type='submit'>Submit The Results</button>
-    
-
-      </form>
+     <form method="POST" action="/" data-netlify="true" onSubmit="submit">
+  <input type="hidden" name="form-name" value="contact" />
+  <div>
+    <label>
+      First Name: <br />
+      <input type="text" name="firstName" />
+    </label>
+  </div>
+  <div>
+    <label>
+      Email: <br />
+      <input type="email" name="email" />
+    </label>
+  </div>
+  <div>
+    <label>
+      Message: <br />
+      <textarea name="message" rows="4" />
+    </label>
+  </div>
+  <button type="submit">Submit</button>
+</form>
     </div>
   );
 }
