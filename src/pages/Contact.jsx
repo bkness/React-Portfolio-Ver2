24684d86c8fd453094dba800695a978f1c5868e8
bkness <kbrandon863@gmail.com>
2024-06-08 02:15:03 -0700

    Added functional contact form with Netlify integration

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,22 +2,36 @@ import React from 'react';
 // import './Contact.css'; 
 
 export default function ContactForm() {
-  return (
-    <>
-      <form name="contact" data-netlify='true'>
-        <p>
-          <label>Name <input type="text" name="contact" /></label>
-        </p>
-        <p>
-          <label>Email <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Messages <input type="text" name="messages" /> </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </>
-  );
+  return <div>
+  
+      <form name="contact v1"
+      method='post'
+      data-netlify='true' 
+      onSubmit="submit"
+      >
+        <input type="hidden" name="form-name" value='contact v1' 
+        />
+
+  <div>
+    <label>First <br />
+      <input type="text" name='first-name' />
+      </label>
+  </div>
+
+  <div>
+    <label htmlFor='email'>Email</label> <br />
+    <input id="email" type="email" name="email" />
+  </div>
+
+  <div>
+    <label>Messages <br />
+<textarea name="messages" rows="7"></textarea>
+    </label>
+  </div>
+
+    <button type='submit'>Submit Message</button>
+    
+    </form> 
+    
+  </div>
 }
