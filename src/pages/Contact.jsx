19dabaf66ad52c0b113f009a298a65be5ab83442
bkness <kbrandon863@gmail.com>
2024-06-08 03:08:21 -0700

    added css to contact page, functionality working in harmony with netlify forms

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
-// import './Contact.css'; 
+import './Contact.css'; 
 
 export default function ContactForm() {
-  return <div>
-  
-      <form name="contact v1"
-      method='post'
-      data-netlify='true' 
-      onSubmit="submit"
+  return (
+    <div className='contact-container'>
+      <h1>Contact</h1>
+      <form 
+        name="contact v1"
+        method='post'
+        data-netlify='true' 
+        onSubmit="submit"
       >
-        <input type="hidden" name="form-name" value='contact v1' 
-        />
+        <input type="hidden" name="form-name" value='contact v1' />
 
-  <div>
-    <label>First <br />
-      <input type="text" name='first-name' />
-      </label>
-  </div>
+        <div>
+          <label className='form-text'>Name: <br />
+            <input className='fields' type="text" name='first-name' />
+          </label>
+        </div>
 
-  <div>
-    <label htmlFor='email'>Email</label> <br />
-    <input id="email" type="email" name="email" />
-  </div>
+        <div>
+          <label className='form-text' htmlFor='email'>Email Address:</label> <br />
+          <input id="email" type="email" name="email"/>
+        </div>
 
-  <div>
-    <label>Messages <br />
-<textarea name="messages" rows="7"></textarea>
-    </label>
-  </div>
+        <div>
+          <label className='form-text'>Messages: <br />
+            <textarea className='fields' name="messages" rows="7"></textarea>
+          </label>
+        </div>
 
-    <button type='submit'>Submit Message</button>
-    
-    </form> 
-    
-  </div>
+        <button type='submit' className='primary-button'>Submit Message</button>
+      </form> 
+    </div>
+  );
 }
