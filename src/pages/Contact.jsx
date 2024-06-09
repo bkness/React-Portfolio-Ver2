426586e086fb79592d7e59a175cb289dee68b749
bkness <kbrandon863@gmail.com>
2024-06-09 14:24:30 -0700

    Update Contact form layout and functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,20 +5,26 @@ export default function Contact() {
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" method="post" data-netlify="true" onSubmit='submit'>
         <input type="hidden" name="form-name" value="contact" />
         <p>
-          <label>Your Name: </label>
-          <input type="text" name="name" />
+          <div>
+            <label>Your Name: </label>
+            <input type="text" name="first-name" />
+          </div>
         </p>
         <p>
-          <label>Your Email: </label>
-          <input type="email" name="email" id="email" />
+          <div>
+            <label htmlFor='email'>Your Email: </label>
+            <input type="email" name="email" id="email" />
+          </div>
         </p>
         <p>
-          <label>Message:
-            <textarea name="message" rows="5"></textarea>
-          </label>
+          <div>
+            <label>Message:
+              <textarea name="message" rows="5"></textarea>
+            </label>
+          </div>
         </p>
         <p>
           <button type="submit">Send</button>
