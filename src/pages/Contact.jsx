fba0c1c97b6a71cfc2e1352b906234f7ce6e567e
bkness <kbrandon863@gmail.com>
2024-06-09 17:32:21 -0700

    updated contact jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Contact.css';
 
-export default function Contact() {
+const Contact = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams(new FormData(form)).toString(),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error));
+  };
+
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <form name="contact" method="POST" data-netlify="true" onSubmit='true'>
+      <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>Your Name: </label>
@@ -26,4 +38,6 @@ export default function Contact() {
       </form>
     </div>
   );
-}
+};
+
+export default Contact;
