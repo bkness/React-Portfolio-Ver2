03a980f58eb1ed34ac55dd61a33fe67c95adbad6
bkness <kbrandon863@gmail.com>
2024-06-12 16:39:31 -0700

    updated contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,10 +14,6 @@ const Contact = () => {
       .catch((error) => alert(error));
   };
 
-  const handleFieldChange = (event) => {
-    event.preventDefault();
-  };
-
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
@@ -25,15 +21,16 @@ const Contact = () => {
         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>Your Name: </label>
-          <input type="text" name="name" onChange={handleFieldChange} />
+          <input type="text" name="name" />
         </p>
         <p>
           <label>Your Email: </label>
-          <input type="email" name="email" id="email" onChange={handleFieldChange} />
+          <input type="email" name="email" id='email' />
         </p>
+  
         <p>
           <label>Message: </label>
-          <textarea name="message" required rows="5" onChange={handleFieldChange}></textarea>
+          <textarea name="message" required rows='5'></textarea>
         </p>
         <p>
           <button type="submit">Send</button>
