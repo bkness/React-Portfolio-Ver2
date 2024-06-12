a2d6a0857d9e2342ade7ff320282aa47bedec4af
bkness <kbrandon863@gmail.com>
2024-06-12 16:53:43 -0700

    current contact layout works with netlify, trying a method with no jsx to reduce code.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,35 +2,35 @@ import React from 'react';
 import './Contact.css';
 
 const Contact = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams(new FormData(form)).toString(),
-    })
-      .then(() => alert('Success!'))
-      .catch((error) => alert(error));
-  };
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const form = event.target;
+  //   fetch('/', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  //     body: new URLSearchParams(new FormData(form)).toString(),
+  //   })
+  //     .then(() => alert('Success!'))
+  //     .catch((error) => alert(error));
+  // };
 
   return (
     <div className="contact-container">
       <h1>Contact Me</h1>
-      <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+      <form name="contact" method="POST" data-netlify="true" onSubmit='submit'>
         <input type="hidden" name="form-name" value="contact" />
         <p>
-          <label>Your Name: </label>
-          <input type="text" name="name" />
+          <label>Name: </label>
+          <input type="text" name="name" placeholder='Your Name' />
         </p>
         <p>
-          <label>Your Email: </label>
-          <input type="email" name="email" id='email' />
+          <label>Email: </label>
+          <input type="email" name="email" placeholder='Your Email' id='email' />
         </p>
   
         <p>
           <label>Message: </label>
-          <textarea name="message" required rows='5'></textarea>
+          <textarea name="message" placeholder='Your Message' required rows='5'></textarea>
         </p>
         <p>
           <button type="submit">Send</button>
