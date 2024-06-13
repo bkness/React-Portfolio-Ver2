11bfcedff57164b1eb1508ead09f6cf5a28f37f6
bkness <kbrandon863@gmail.com>
2024-06-13 14:37:56 -0700

    updated projects with group bootcamp projects, added roles

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,8 +4,8 @@ import './Resume.css';
 export default function Resume() {
   return (
     <div className="resume-container"> <br />
-       <div className="contact-info">
-      <h1 className="name">Brandon Michael Kelly</h1> <br />
+      <div className="contact-info">
+        <h1 className="name">Brandon Michael Kelly</h1> <br />
         <div>
           Cottonwood, AZ 86326 | Phone: 928-274-6725<br />
         </div>
@@ -26,31 +26,43 @@ export default function Resume() {
         <strong className="skill-def">Other:</strong> Responsive Design, RESTful APIs, Agile Methodologies
       </p> <br />
 
-      <h2 className="section-title">Related Experiences</h2> <br />
+      <h2 className="section-title">Related Experience</h2> <br />
       <div className="project">
-        <strong className="project-description">Arizona State University Web Development Program</strong> 
+        <strong className="project-description">Arizona State University Web Development Program</strong>
       </div><br />
+
+      <div className="project">
+        <strong className="project-description">Local Brewery Finder</strong> |
+        <a href="https://github.com/bkness/LocalBreweries" target="_blank" rel="noopener noreferrer">GitHub</a> |
+        <a href="https://radiant-fjord-87739-920e7bb79c22.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live</a><br />
+        Developed a full-stack application for users to post and comment on tech articles.<br />
+        <strong className="project-description">Responsibilities:</strong> Developed a web application integrating an API to find local breweries, allowing users to log in, save favorite breweries, and manage their personalized lists <br />
+        <strong className="project-description">Role:</strong> RESTful API integration of Open Brewery DB, Map functionality when ‘view map’ for the brewery is pressed, CSS <br />
+        <strong className="project-description">Technologies | Languages:</strong> Node.js, Express.js, RESTful API, Handlebars, SQL, NPM, HTML
+      </div> <br />
+
+      <div className="project">
+        <strong className="project-description">NBA Standings Application</strong> |
+        <a href="https://github.com/bkness/jungle-map-api " target="_blank" rel="noopener noreferrer">GitHub</a> |
+        <a href="https://bkness.github.io/jungle-map-api/" target="_blank" rel="noopener noreferrer">Live</a><br />
+        Built a web application to generate random cocktail recipes using an external API. <br />
+        <strong className="project-description">Responsibilities:</strong> Responsive sports standings app using Tailwind, integrating two APIs for real-time data and implementing client-side storage for persistent data <br />
+        <strong className="project-description">Role:</strong> Integration of Sports API, Modal design and functionality, CSS <br />
+        <strong className="project-description">Technologies | Languages</strong> Server Side API, Local Storage, Tailwind CSS, HTML5
+      </div> <br />
 
       <div className="project">
         <strong className="project-description">Tech Blog</strong> |
         <a href="https://github.com/bkness/MVC-Tech-Blog" target="_blank" rel="noopener noreferrer">GitHub</a> |
         <a href="https://mvc--tech-blog-715ec89956fb.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live</a><br />
         Developed a full-stack application for users to post and comment on tech articles.<br />
-        <strong className="project-description">Responsibilities:</strong> Created front-end interface with Handlebars.js, implemented user authentication, and set up RESTful API routes.<br />
-        <strong className="project-description">Technologies:</strong> Node.js, Express, MySQL, Handlebars.js
+        <strong className="project-description">Responsibilities:</strong> Created front-end interface with Handlebars.js, implemented user authentication, and set up RESTful API routes. <br />
+        <strong className="project-description">Role:</strong> Integrated MySQL2 and Sequelize for robust user information management, implemented user authentication, and utilized Handlebars template engine for dynamic data rendering. <br />
+        <strong className="project-description">Technologies | Languages</strong> Express, Handlebars, MySQL, Sequelize, Bcrypt, dotenv, and Google Fonts
       </div> <br />
 
       <div className="project">
-        <strong className="project-description">Random Cocktail Generator</strong> |
-        <a href="https://github.com/bkness/Random-Cocktail-API" target="_blank" rel="noopener noreferrer">GitHub</a> |
-        <a href="https://bkness.github.io/Random-Cocktail-API" target="_blank" rel="noopener noreferrer">Live</a><br />
-        Built a web application to generate random cocktail recipes using an external API.<br />
-        <strong className="project-description">Responsibilities:</strong> Designed UI with HTML and CSS, integrated API with JavaScript, and ensured responsive design.<br />
-        <strong className="project-description">Technologies:</strong> HTML, CSS, JavaScript, REST API
-      </div> <br />
-
-      <div className="project">
-        <strong className="project-description">Open Weather Database</strong> |
+        <strong className="project-description">Open Weather Dashboard</strong> |
         <a href="https://github.com/bkness/open-weather-api" target="_blank" rel="noopener noreferrer">GitHub</a> |
         <a href="https://bkness.github.io/open-weather-api" target="_blank" rel="noopener noreferrer">Live</a><br />
         Created a weather application that fetches and displays weather data for user-selected cities.<br />
@@ -60,7 +72,7 @@ export default function Resume() {
 
       <h2 className="section-title">Experience</h2> <br />
 
-      <div className="experience"> 
+      <div className="experience">
         <strong className="project-description">Creekside American Bistro, Bartender</strong> | <strong className="date">March 2019 – January 2021</strong><br />
         <div className="location"> Cottonwood, AZ<br /></div> <br />
         Crafted and served signature cocktails, maintained bar inventory, and ensured exceptional customer service.<br />
