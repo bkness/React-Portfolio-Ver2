80be2a6f2a60a3da6a197e85ab9d2019230f85b4
bkness <kbrandon863@gmail.com>
2024-06-13 19:20:30 -0700

    Added project 3 to my resume and commented out weather dashboard, updated resume css

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -24,7 +24,7 @@ export default function Resume() {
         <a href="https://www.linkedin.com/in/brandon-kelly-367b3730b/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         <div className="separator">|</div>
         <a href="https://github.com/bkness" target="_blank" rel="noopener noreferrer">GitHub</a>
-      </div><br /> <br />
+      </div><br /> 
 
       <h2 className="section-title">Summary</h2> <br />
 
@@ -35,8 +35,8 @@ export default function Resume() {
       <p className="skills">
         <strong className="skill-def">Languages:</strong> HTML5, CSS3, JavaScript, React<br />
         <strong className="skill-def">Frameworks & Libraries:</strong> Bootstrap, Tailwind, jQuery, Redux, Handlebars.js, Node.js<br />
-        <strong className="skill-def">Tools & Platforms:</strong> Git, GitHub, VS Code, Heroku, MongoDB, GraphQL, Netlify, Render, Firebase<br />
-        <strong className="skill-def">Other:</strong> Responsive Design, RESTful APIs, Agile Methodologies
+        <strong className="skill-def">Tools & Platforms:</strong> Git, GitHub, VS Code, Heroku, MongoDB, Netlify, Render, Firebase<br />
+        <strong className="skill-def">Other:</strong> GraphQL, Responsive Design, RESTful APIs, Agile Methodologies
       </p> <br />
 
       <h2 className="section-title">Related Experience</h2> <br />
@@ -46,39 +46,48 @@ export default function Resume() {
       </div><br />
 
       <div className="project">
+        <strong className="project-description">Video-Game-Hub</strong> |
+        <a href="https://github.com/bkness/LocalBreweries" target="_blank" rel="noopener noreferrer">GitHub</a> |
+        <a href="https://radiant-fjord-87739-920e7bb79c22.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live</a><br /> <br />
+        <strong className="project-description">Responsibilities:</strong> Collaborated on the development of a MERN stack application, utilizing MongoDB for the backend, GraphQL API, and Express.js/Node.js. Developed the front end with JWT authentication, Agile methodologies, and deployment to Render <br />
+        <strong className="project-description">Role:</strong> Developed the blog section, integrated GraphQL with type definitions and mutations, tested with Apollo, and contributed to CSS styling <br />
+        <strong className="project-description">Technologies | Languages:</strong> MongoDB, GraphQL, Express.js, Node.js, React.js, JWT, Apollo, Agile, Render, CSS
+      </div> <br />
+
+      <div className="project">
         <strong className="project-description">Local Brewery Finder</strong> |
         <a href="https://github.com/bkness/LocalBreweries" target="_blank" rel="noopener noreferrer">GitHub</a> |
-        <a href="https://radiant-fjord-87739-920e7bb79c22.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live</a><br />
+        <a href="https://radiant-fjord-87739-920e7bb79c22.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live</a><br /> <br />
         <strong className="project-description">Responsibilities:</strong> Developed a web application integrating an API to find local breweries, allowing users to log in, save favorite breweries, and manage their personalized lists <br />
         <strong className="project-description">Role:</strong> RESTful API integration of Open Brewery DB, Map functionality when ‘view map’ for the brewery is pressed, CSS <br />
-        <strong className="project-description">Technologies | Languages:</strong> Node.js, Express.js, RESTful API, Handlebars, SQL, NPM, HTML
+        <strong className="project-description">Technologies | Languages:</strong> Node.js, Express.js, RESTful API, Handlebars, SQL, NPM, Heroku, CSS, Google Fonts
       </div> <br />
 
       <div className="project">
         <strong className="project-description">NBA Standings Application</strong> |
         <a href="https://github.com/bkness/jungle-map-api " target="_blank" rel="noopener noreferrer">GitHub</a> |
-        <a href="https://bkness.github.io/jungle-map-api/" target="_blank" rel="noopener noreferrer">Live</a><br />
+        <a href="https://bkness.github.io/jungle-map-api/" target="_blank" rel="noopener noreferrer">Live</a><br /> <br />
         <strong className="project-description">Responsibilities:</strong> Responsive sports standings app using Tailwind, integrating two APIs for real-time data and implementing client-side storage for persistent data <br />
         <strong className="project-description">Role:</strong> Integration of Sports API, Modal design and functionality, CSS <br />
-        <strong className="project-description">Technologies | Languages</strong> Server Side API, Local Storage, Tailwind CSS, HTML5
+        <strong className="project-description">Technologies | Languages</strong> Server Side API, Local Storage, Tailwind CSS, HTML
       </div> <br />
 
       <div className="project">
         <strong className="project-description">Tech Blog</strong> |
         <a href="https://github.com/bkness/MVC-Tech-Blog" target="_blank" rel="noopener noreferrer">GitHub</a> |
-        <a href="https://mvc--tech-blog-715ec89956fb.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live</a><br />
-        <strong className="project-description">Responsibilities:</strong> Created front-end interface with Handlebars.js, implemented user authentication, and set up RESTful API routes. <br />
-        <strong className="project-description">Role:</strong> Integrated MySQL2 and Sequelize for robust user information management, implemented user authentication, and utilized Handlebars template engine for dynamic data rendering. <br />
+        <a href="https://mvc--tech-blog-715ec89956fb.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live</a><br /> <br />
+        <strong className="project-description">Responsibilities:</strong> Created front-end interface with Handlebars.js, implemented user authentication, and set up RESTful API routes <br />
+        <strong className="project-description">Role:</strong> Integrated MySQL2 and Sequelize for robust user information management, implemented user authentication, and utilized Handlebars template engine for dynamic data rendering <br />
         <strong className="project-description">Technologies | Languages</strong> Express, Handlebars, MySQL, Sequelize, Bcrypt, dotenv, and Google Fonts
       </div> <br />
 
-      <div className="project">
+      {/* <div className="project">
         <strong className="project-description">Open Weather Dashboard</strong> |
         <a href="https://github.com/bkness/open-weather-api" target="_blank" rel="noopener noreferrer">GitHub</a> |
         <a href="https://bkness.github.io/open-weather-api" target="_blank" rel="noopener noreferrer">Live</a><br />
         <strong className="project-description">Responsibilities:</strong> Developed front-end with HTML, CSS, and JavaScript, implemented API calls, and handled data presentation.<br />
         <strong className="project-description">Technologies:</strong> HTML, CSS, JavaScript, OpenWeatherAPI
-      </div> <br /> <br />
+      </div> <br /> <br /> */} <br />
 
       <h2 className="section-title">Experience</h2> <br />
 
