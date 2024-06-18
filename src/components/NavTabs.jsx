51d30935699cb5311cf0f4b3f6a6d24b11006bb3
bkness <kbrandon863@gmail.com>
2024-06-18 01:19:21 -0700

    re did nav bar component and css, reducing code

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,77 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
 import { FaHome, FaUser, FaBriefcase, FaEnvelope } from 'react-icons/fa';
 import { Link, useLocation } from 'react-router-dom';
 import './NavTabs.css';
 
-function NavTabs() {
-  const currentPage = useLocation().pathname;
+export default function NavTabs() {
+const [isOpen, setIsOpen] = useState(false);
 
-  const handleNavLinkClick = () => {
-    const navbarToggler = document.getElementById('navbarToggler');
-    if (navbarToggler && navbarToggler.getAttribute('aria-expanded') === 'true') {
-      navbarToggler.click();
-    }
-  };
-
+const toggleMenu = () => {
+  setIsOpen((open) => !open)
+}
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container-fluid">
-        <Link className="my-name" to="/">Brandon Kelly</Link>
-        <button
-          id="navbarToggler"
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link
-                to="/"
-                className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-                onClick={handleNavLinkClick}
-              >
-                <FaHome className="react-icon" /> Home
-              </Link>
+   <nav className='dvs-header'>
+      <ul className='d-flex wrap justify-between align-center'>
+        <li>
+          <a href="#" className='nav-text my-name'>Brandon Kelly</a>
+        </li>
+        <li className='dvs-header__trigger' onClick={toggleMenu}>
+            Trigger
             </li>
-            <li className="nav-item">
-              <Link
-                to="/Portfolio"
-                className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
-                onClick={handleNavLinkClick}
-              >
-                <FaBriefcase className="react-icon" /> Portfolio
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/Resume"
-                className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
-                onClick={handleNavLinkClick}
-              >
-                <FaUser className="react-icon" /> Resume
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                to="/Contact"
-                className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
-                onClick={handleNavLinkClick}
-              >
-                <FaEnvelope className="react-icon" /> Contact
-              </Link>
-            </li>
+        <li class={`dvs-header__menuItems ${isOpen ? 'is-open' : ''}`}>
+        <ul>
+          <li className='d-flex align-center wrap'>
+            <a href='#' className='nav-text'>About</a>
+            <a href='#' className='nav-text'>Portfolio</a>
+            <a href='#' className='nav-text'>Contact</a>
+          </li>
           </ul>
-        </div>
-      </div>
+         </li> 
+      </ul>
     </nav>
   );
 }
 
-export default NavTabs;
+
