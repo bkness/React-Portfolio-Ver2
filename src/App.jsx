d6d05080c4b6adbeddd4e1781d9ffed2ed23eb82
bkness <kbrandon863@gmail.com>
2024-06-19 16:04:23 -0700

    adjusted css for entire app, followed 3 color rule.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom';
 import NavTabs from './components/NavTabs';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 
 function App() {
