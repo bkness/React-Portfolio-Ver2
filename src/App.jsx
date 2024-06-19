1b6ae295349098a66d79356f9d080f6392f59fd5
bkness <kbrandon863@gmail.com>
2024-06-19 16:46:37 -0700

    updated bootstrap in app.jsx, adjusted hamburger color

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import NavTabs from './components/NavTabs';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 
 function App() {
