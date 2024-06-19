d5b7265b3b1c7dbf839fca583da67f5f5704f0d2
bkness <kbrandon863@gmail.com>
2024-06-18 17:21:03 -0700

    removed old contact container, utilizing honeypot and all fields required. more polished / clean

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,26 +19,3 @@ const Contact = () => {
 
 export default Contact;
 
-
-{/* <div className="contact-container">
-<h1>Contact Me</h1>
-<form name="contact" method="POST" data-netlify="true" onSubmit='submit'>
-  <input type="hidden" name="form-name" value="contact" />
-  <p>
-    <label>Name: </label>
-    <input type="text" name="name" placeholder='Your Name' />
-  </p>
-  <p>
-    <label>Email: </label>
-    <input type="email" name="email" placeholder='Your Email' id='email' />
-  </p>
-
-  <p>
-    <label>Message: </label>
-    <textarea name="message" placeholder='Your Message' required rows='5'></textarea>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-</div> */}
